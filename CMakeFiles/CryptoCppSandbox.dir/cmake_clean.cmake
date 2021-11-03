0cce268e6919c73bcd6118937222d7943070e78d
Kim Savaroche <kim@kims-macbook-air.home>
2021-11-03 09:58:17 +0100

    Add all

---
+++ CMakeFiles/CryptoCppSandbox.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CryptoCppSandbox.dir/main.cpp.o"
+  "CMakeFiles/CryptoCppSandbox.dir/main.cpp.o.d"
+  "CryptoCppSandbox"
+  "CryptoCppSandbox.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CryptoCppSandbox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/CryptoCppSandbox.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for CryptoCppSandbox.
+# This may be replaced when dependencies are built.
